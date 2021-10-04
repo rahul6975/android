3c2e5a98e4f73bf11246afbd4d709f350b458779
Christian Schabesberger <chris.schabesberger@mailbox.org>
2021-10-04 11:09:46 +0200

    add stetho for debug

--- owncloudApp/src/main/java/com/owncloud/android/MainApp.kt
+++ owncloudApp/src/main/java/com/owncloud/android/MainApp.kt
@@ -50,11 +50,7 @@ import com.owncloud.android.ui.activity.BiometricActivity
 import com.owncloud.android.presentation.ui.security.PassCodeActivity
 import com.owncloud.android.ui.activity.PatternLockActivity
 import com.owncloud.android.ui.activity.WhatsNewActivity
-import com.owncloud.android.utils.DOWNLOAD_NOTIFICATION_CHANNEL_ID
-import com.owncloud.android.utils.FILE_SYNC_CONFLICT_CHANNEL_ID
-import com.owncloud.android.utils.FILE_SYNC_NOTIFICATION_CHANNEL_ID
-import com.owncloud.android.utils.MEDIA_SERVICE_NOTIFICATION_CHANNEL_ID
-import com.owncloud.android.utils.UPLOAD_NOTIFICATION_CHANNEL_ID
+import com.owncloud.android.utils.*
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
@@ -74,6 +70,8 @@ class MainApp : Application() {
         appContext = applicationContext
 
         startLogsIfEnabled()
+
+        DebugInjector.injectDebugTools(appContext)
 
         OwnCloudClient.setContext(appContext)
 
